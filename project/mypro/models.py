16290c0e0147f0b3fbe338d8c343e0cad93b0bf5
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-22 10:19:59 +0530

    Django T4

---
+++ project/mypro/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    Name=models.CharField()
+    DOB=models.DateField()
+    enrollment=models.IntegerField()
+    project_title=models.CharField()
+    def __str__(self):
+        return self.Name
---
+++ project/mypro/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Student
+# Create your views here.
+
+def display(request):
+    data=Student.objects.all()
+    return render(request,"1.html",{"new":data})
+
+def update(request,id):
+    d=get_object_or_404(Student,id=id)
+    if request.method=="POST":
+        d.Name=request.POST.get("Name")
+        d.DOB=request.POST.get("DOB")
+        d.enrollment=request.POST.get("enrollment")
+        d.project_title=request.POST.get("project_title")
+        d.save()
+        return redirect("display")
+    
+    return render(request,"update.html",{"d":d})
